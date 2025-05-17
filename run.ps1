a24554fcae5d6c9aaea38b50a855328967cdd903
far14c <farhanafnaniskandar@gmail.com>
2025-05-17 23:10:09 +0700

    Added configuration for local deployment

--- run.ps1
+++
@@ -1,8 +0,0 @@
-$env:DB_HOST="sql12.freesqldatabase.com"
-$env:DB_PORT="3306"
-$env:DB_USER="sql12779376"
-$env:DB_PASSWORD="qxYfWPepwX"
-$env:DB_NAME="sql12779376"
-$env:SERVER_PORT="8080"
-
-.\voucher-api.exe 
